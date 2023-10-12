5da04407ed5bff024ddc607a677f1481fe36afb2
Stimarc <rikudo7474@gmail.net>
2023-10-12 18:02:51 +0300

    update

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,20 +3,20 @@ import Header from './components/Header';
 const App = () => {
   const title = 'My site from React !!!';
   const isDark = true;
+
   return (
     <div className="App">
-    
-    <Header />
 
-    <main className="main">
-      <h1 
-      className={ isDark ? 'main-title main-title_dark' : 'main-title'}
-       >
-        
-        [title]
+      <Header />
+      
+      <main className="main">
+        <h1 
+          className={ isDark ? 'main-title main-title_dark' : 'main-title'}
+        >
+          { title }
         </h1>
-    </main>
-    <footer className="footer"></footer>
+      </main>
+      <footer className="footer"></footer>
     </div>
   );
 }
